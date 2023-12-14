6184622515ddcdf197288057e027c8700321a883
최규원 <72799542+GYUWON-CHOI@users.noreply.github.com>
2023-12-14 23:14:14 +0900

    Update TFTPclient.py

--- TFTPclient.py
+++ TFTPclient.py
@@ -5,7 +5,6 @@ $ tftp ip_address [-p port_mumber] <get|put> filename
 import sys
 import socket
 import argparse
-#import validators
 from struct import pack
 
 DEFAULT_PORT = 69
@@ -13,7 +12,7 @@ BLOCK_SIZE = 512
 DEFAULT_TRANSFER_MODE = 'octet'
 
 OPCODE = {'RRQ': 1, 'WRQ': 2, 'DATA': 3, 'ACK': 4, 'ERROR': 5}
-MODE = {'netascii': 1,'octet': 2, 'mail': 3}
+MODE = {'netascii': 1, 'octet': 2, 'mail': 3}
 
 ERROR_CODE = {
     0: "Not defined, see error message (if any).",
@@ -51,7 +50,7 @@ def send_data(block_number, file_block, server_new_socket):
     sock.sendto(data_packet, server_new_socket)
     print(block_number)
 
-# parse command line arguments
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='TFTP client program')
 parser.add_argument(dest="host", help="Server IP address", type=str)
 parser.add_argument(dest="operation", help="get or put a file", type=str)
@@ -61,13 +60,14 @@ args = parser.parse_args()
 
 '''
 if validators.domain(args.host):
-    serber_ip = gethostbyname(args.host)
+    serber_ip = gethostbyname(args.host) 
 else
     server_ip = args.host
 
 if args.port == None:
     server_port = DEFAULT_PORT
 '''
+
 # Create a UDP socket
 server_ip = args.host
 server_port = DEFAULT_PORT
@@ -79,6 +79,7 @@ operation = args.operation
 filename = args.filename
 
 if operation.lower() == 'put':
+    sock.settimeout(5)
     send_wrq(filename, mode)
     file = open(filename, 'rb')
     blnum = 1
@@ -87,51 +88,51 @@ elif operation.lower() == 'get':
     file = open(filename, 'wb')
     expected_block_number = 1
 else:
-    print("유효하지 않은 작업입니다. 'put' 또는 'get'을 사용하십시오.")
+    print("Invalid operation. Please use 'put' or 'get'.")
 
 while True:
-    # receive data from the server
-    # server uses a newly assigned port(not 69)to transfer data
-    # so ACK should be sent to the new socket
-    data, server_new_socket = sock.recvfrom(516)
-    opcode = int.from_bytes(data[:2], 'big')
+    try:
+        # Receive data from the server
+        # Server uses a newly assigned port (not 69) to transfer data
+        # So ACK should be sent to the new socket
+        data, server_new_socket = sock.recvfrom(516)
+        opcode = int.from_bytes(data[:2], 'big')
 
-    # check message type
-    if opcode == OPCODE['DATA']:
-        block_number = int.from_bytes(data[2:4], 'big')
-        if block_number == expected_block_number:
-            send_ack(block_number, server_new_socket)
-            file_block = data[4:]
-            file.write(file_block)
-            expected_block_number = expected_block_number +1
-            print(file_block.decode())
+        # Check message type
+        if opcode == OPCODE['DATA']:
+            block_number = int.from_bytes(data[2:4], 'big')
+            if block_number == expected_block_number:
+                send_ack(block_number, server_new_socket)
+                file_block = data[4:]
+                file.write(file_block)
+                expected_block_number += 1
+                print(file_block.decode())
+            else:
+                send_ack(block_number, server_new_socket)
+
+        elif opcode == OPCODE['ACK']:
+            block_number = int.from_bytes(data[2:4], 'big')
+            file_block = file.read(512)
+            if block_number == 0 or block_number == blnum:
+                send_data(blnum, file_block, server_new_socket)
+                blnum += 1
+                if not file_block:
+                    break
+            else:
+                send_data(blnum, file_block, server_new_socket)
+
+        elif opcode == OPCODE['ERROR']:
+            error_code = int.from_bytes(data[2:4], byteorder='big')
+            print(ERROR_CODE[error_code])
+            break
+
         else:
-            send_ack(block_number, server_new_socket)
+            break
 
-    elif opcode == OPCODE['ACK']:
-        block_number = int.from_bytes(data[2:4], 'big')
-        file_block = file.read(512)
-        if block_number == 0:
-            send_data(blnum, file_block, server_new_socket)
-            blnum = blnum + 1
-        elif block_number == blnum:
-            send_data(blnum, file_block, server_new_socket)
-            blnum = blnum + 1
-            if not file_block:
-                break
-        else:
-            send_data(blnum, file_block, server_new_socket)
-
-
-    elif opcode == OPCODE['ERROR']:
-        error_code = int.from_bytes(data[2:4], byteorder='big')
-        print(ERROR_CODE[error_code])
-        break
-
-    else:
-        break
-
-    if len(file_block) < BLOCK_SIZE:
-        file.close()
-        print(len(file_block))
+        if len(file_block) < BLOCK_SIZE:
+            file.close()
+            print(len(file_block))
+            break
+    except socket.timeout:
+        print("Socket Timeout")
         break
