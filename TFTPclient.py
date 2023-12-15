b549c09f3cf7788844cd6f828453de3b407c2fdd
최규원 <72799542+GYUWON-CHOI@users.noreply.github.com>
2023-12-15 17:50:05 +0900

    Update TFTPclient.py

--- TFTPclient.py
+++ TFTPclient.py
@@ -25,27 +25,27 @@ ERROR_CODE = {
     7: "No such user."
 }
 
-def send_wrq(filename, mode):
-    format = f'>h{len(filename)}sB{len(mode)}sB'
-    wrq_message = pack(format, OPCODE['WRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
-    sock.sendto(wrq_message, server_address)
+def send_wrq(filename, mode): # WRQ 패킷 전송
+    format = f'>h{len(filename)}sB{len(mode)}sB' # 포맷 지정
+    wrq_message = pack(format, OPCODE['WRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0) # WRQ 패킷 생성
+    sock.sendto(wrq_message, server_address) # 서버에 전송
     print(wrq_message)
 
-def send_rrq(filename, mode):
+def send_rrq(filename, mode): # RRQ 패킷 전송
     format = f'>h{len(filename)}sB{len(mode)}sB'
     rrq_message = pack(format, OPCODE['RRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
     sock.sendto(rrq_message, server_address)
     print(rrq_message)
 
-def send_ack(seq_num, server):
+def send_ack(seq_num, server): # ACK 패킷 전송
     format = f'>hh'
     ack_message = pack(format, OPCODE['ACK'], seq_num)
     sock.sendto(ack_message, server)
     print(seq_num)
     print(ack_message)
 
-def send_data(block_number, file_block, server_new_socket):
-    sock.settimeout(5)
+def send_data(block_number, file_block, server_new_socket): # 데이터 패킷 전송
+    sock.settimeout(5) # 소켓 타임아웃 설정
     max_retries = 3  # 최대 재시도 횟수
     retries = 0
 
@@ -109,36 +109,36 @@ while True:
     opcode = int.from_bytes(data[:2], 'big')
 
     # Check message type
-    if opcode == OPCODE['DATA']:
-        block_number = int.from_bytes(data[2:4], 'big')
-        if block_number == expected_block_number:
-            send_ack(block_number, server_new_socket)
-            file_block = data[4:]
-            file.write(file_block)
-            expected_block_number += 1
+    if opcode == OPCODE['DATA']: # 데이터 패킷을 수신했을 
+        block_number = int.from_bytes(data[2:4], 'big') # 블록 번호 추출
+        if block_number == expected_block_number: # 예상 블록 번호와 일치하는 경우
+            send_ack(block_number, server_new_socket) # ACK 전송
+            file_block = data[4:] # 데이터 블록 추출
+            file.write(file_block) # 쓰기
+            expected_block_number += 1 # 예상 번호 증가
             print(file_block.decode())
         else:
             send_ack(block_number, server_new_socket)
 
-    elif opcode == OPCODE['ACK']:
-        block_number = int.from_bytes(data[2:4], 'big')
-        file_block = file.read(512)
-        if block_number == 0 or block_number == blnum:
-            send_data(blnum, file_block, server_new_socket)
-            blnum += 1
-            if not file_block:
-                break
+    elif opcode == OPCODE['ACK']: # ACK을 수신했을 때
+        block_number = int.from_bytes(data[2:4], 'big') # 블록 번호 추출
+        file_block = file.read(512) # 데이터 블록 읽기
+        if block_number == 0 or block_number == blnum: # 블록 번호가 0이거나 예상 블록 번화와 일치하는 경
+            send_data(blnum, file_block, server_new_socket) # 데이터 전
+            blnum += 1 # 예상 번호 증가
+            if not file_block: # 파일 끝에 도달할 경우
+                break # 루프 종료
         else:
             send_data(blnum, file_block, server_new_socket)
 
-    elif opcode == OPCODE['ERROR']:
+    elif opcode == OPCODE['ERROR']: # 오류 패킷을 수신했을 때
         error_code = int.from_bytes(data[2:4], byteorder='big')
         if error_code == 1:  # File not found
-            print(ERROR_CODE[error_code])
-            sys.exit()
+            print(ERROR_CODE[error_code]) # 오류 메시지 출력
+            sys.exit() # 프로그램 종료
         else:
-            print(ERROR_CODE[error_code])
-            sys.exit()
+            print(ERROR_CODE[error_code]) # 오류 메시지 출력
+            sys.exit() # 프로그램 종료
 
     else:
         break
